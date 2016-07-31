d8cc058d6d636afba08d4df79359b01047cf6703
junhyuk <junhyuk@umich.edu>
2016-07-31 02:01:34 -0400

    Add progress bar, don't save agent

--- train.lua
+++ train.lua
@@ -19,7 +19,6 @@ cmd:option('-framework', 'environment.mcwrap', 'name of training framework')
 cmd:option('-env', '', 'name of environment to use')
 cmd:option('-test_env', '', 'name of test environment to use')
 cmd:option('-test_hist_len', 30, 'history length for testing')
-cmd:option('-test_freq', 1e5, 'frequency of test')
 cmd:option('-test_steps', 1e4, 'number of evaluation steps for test environment')
 cmd:option('-env_params', '', 'string of environment parameters')
 cmd:option('-actrep', 1, 'how many times to repeat action')
@@ -29,11 +28,10 @@ cmd:option('-agent', 'NeuralQLearner', 'name of agent file to use')
 cmd:option('-agent_params', '', 'string of agent parameters')
 cmd:option('-seed', 1, 'fixed input seed for repeatable experiments')
 cmd:option('-saveNetworkParams', true, 'saves the agent network in a separate file')
-cmd:option('-prog_freq', 1e4, 'frequency of progress output')
 cmd:option('-save_freq', 1e5, 'the model is saved every save_freq steps')
 cmd:option('-eval_freq', 1e5, 'frequency of greedy evaluation')
-cmd:option('-steps', 15e6, 'number of training steps to perform')
 cmd:option('-eval_steps', 1e4, 'number of evaluation steps')
+cmd:option('-steps', 15e6, 'number of training steps to perform')
 cmd:option('-verbose', 2,
            'the higher the level, the more information is printed to screen')
 cmd:option('-threads', 4, 'number of BLAS threads')
@@ -119,14 +117,12 @@ local epoch_time = sys.clock()
 local ep_reward = 0
 local ep_step = 0
 local ev_flag = false
-local prog_flag = false
-local test_flag = false
 os.execute("mkdir -p save")
+local epoch = 0
 while step < opt.steps do
     step = step + 1
+    xlua.progress(math.min(step - epoch * opt.eval_freq, opt.eval_freq), opt.eval_freq)
     if step % opt.eval_freq == 0 and step > learn_start then ev_flag = true end
-    if step % opt.test_freq == 0 and step > learn_start then test_flag = true end
-    if step % opt.prog_freq == 0 then prog_flag = true end
     local action_index = agent:perceive(reward, screen, terminal)
     screen, reward, terminal = game_env:step(game_actions[action_index], true)
     ep_reward = ep_reward + reward
@@ -134,16 +130,20 @@ while step < opt.steps do
 
     if terminal then
         step = step + 1
-        local epoch = torch.floor(step/opt.eval_freq)
         local action_index = agent:perceive(reward, screen, terminal)
         if step % opt.eval_freq == 0 and step > learn_start then ev_flag = true end
-        if step % opt.test_freq == 0 and step > learn_start then test_flag = true end
-        if step % opt.prog_freq == 0 then prog_flag = true end
         screen, reward, terminal = game_env:newGame()
         ep_reward = 0
         ep_step = 0
         if ev_flag then
-            print("Evalutating the agent on the training environment: " 
+            epoch = epoch + 1
+            epoch_time = sys.clock() - epoch_time
+            print("Steps:", step, "Steps/Sec:",  math.floor(opt.eval_freq / epoch_time))
+            if opt.verbose > 2 then
+                agent:report()
+            end
+            epoch_time = sys.clock()
+            print("Evaluating the agent on the training environment: " 
                     .. color.green(train_env))
             ev_flag = false
             nepisodes, total_reward = eval_agent(game_env, agent, opt.eval_steps)
@@ -158,80 +158,60 @@ while step < opt.steps do
             reward_history[ind] = total_reward
             episode_counts[ind] = nepisodes
             screen, reward, terminal = game_env:newGame()
-        end
-
-        if test_flag and #test_env > 0 then
-            test_flag = false
-            if not agent.best_test_network then
-                agent.best_test_network = {}
-            end
-            for test_id=1,#test_env do
-                local ind = #test_reward_history[test_id]+1
-                print("Evalutating the agent on the test environment: " 
-                        .. color.green(test_env_names[test_id]))
-                nepisodes, total_reward = eval_agent(test_env[test_id], test_agent, opt.test_steps)
-                total_reward = total_reward/math.max(1, nepisodes)
-                if #test_reward_history[test_id] == 0 or 
-                        total_reward > torch.Tensor(test_reward_history[test_id]):max() then
-                    agent.best_test_network[test_id] = test_agent.network:clone()
+            if #test_env > 0 then
+                if not agent.best_test_network then
+                    agent.best_test_network = {}
                 end
-                test_reward_history[test_id][ind] = total_reward
-                print("Epoch:", epoch, "Reward:", total_reward, "num. ep.:", nepisodes)
-            end
-
-            -- Maintain and save only top K best models
-            if opt.saveNetworkParams then
-                local test_reward = test_reward_history[1][#test_reward_history[1]]
-                local min_key = -1
-                local min_instance = nil
-                for k, v in pairs(best_history) do
-                    if min_instance == nil or min_instance.reward > v.reward then
-                        min_key = k
-                        min_instance = v
+                for test_id=1,#test_env do
+                    local ind = #test_reward_history[test_id]+1
+                    print("Evaluating the agent on the test environment: " 
+                            .. color.green(test_env_names[test_id]))
+                    nepisodes, total_reward = eval_agent(test_env[test_id], test_agent, opt.test_steps)
+                    total_reward = total_reward/math.max(1, nepisodes)
+                    if #test_reward_history[test_id] == 0 or 
+                            total_reward > torch.Tensor(test_reward_history[test_id]):max() then
+                        agent.best_test_network[test_id] = test_agent.network:clone()
                     end
+                    test_reward_history[test_id][ind] = total_reward
+                    print("Epoch:", epoch, "Reward:", total_reward, "num. ep.:", nepisodes)
                 end
-                if #best_history < opt.num_save or min_instance.reward < test_reward then
-                    if min_instance and #best_history >= opt.num_save then
-                        os.execute("rm " .. string.format("save/%s_%03d.params.t7", 
-                            opt.save_name, min_instance.epoch))
-                        table.remove(best_history, min_key)
+
+                -- Maintain and save only top K best models
+                if opt.saveNetworkParams then
+                    local test_reward = test_reward_history[1][#test_reward_history[1]]
+                    local min_key = -1
+                    local min_instance = nil
+                    for k, v in pairs(best_history) do
+                        if min_instance == nil or min_instance.reward > v.reward then
+                            min_key = k
+                            min_instance = v
+                        end
                     end
+                    if #best_history < opt.num_save or min_instance.reward < test_reward then
+                        if min_instance and #best_history >= opt.num_save then
+                            os.execute("rm " .. string.format("save/%s_%03d.params.t7", 
+                                opt.save_name, min_instance.epoch))
+                            table.remove(best_history, min_key)
+                        end
 
-                    local test_instance = {}
-                    test_instance.epoch = epoch
-                    test_instance.reward = test_reward
-                    table.insert(best_history, test_instance)
+                        local test_instance = {}
+                        test_instance.epoch = epoch
+                        test_instance.reward = test_reward
+                        table.insert(best_history, test_instance)
+                    end
+                    local filename = string.format('save/%s_%03d.params.t7', opt.save_name, epoch)
+                    torch.save(filename, agent.w:clone():float())
+                    print('Parameter saved to:', filename)
                 end
-                local filename = string.format('save/%s_%03d.params.t7', opt.save_name, epoch)
-                torch.save(filename, agent.w:clone():float())
-                print('Parameter saved to:', filename)
+                collectgarbage()
             end
-            collectgarbage()
         end
-    end
-
-    if prog_flag then
-        prog_flag = false
-        epoch_time = sys.clock() - epoch_time
-        assert(step==agent.numSteps, 'trainer step: ' .. step ..
-                ' & agent.numSteps: ' .. agent.numSteps)
-        print("Steps:", step, "Steps/Sec:",  math.floor(opt.prog_freq / epoch_time))
-        if opt.verbose > 2 then
-            agent:report()
-        end
-        epoch_time = sys.clock()
     end
 
     if step%1000 == 0 then collectgarbage() end
     if step % opt.save_freq == 0 or step == opt.steps then
-        local w, dw, g, g2, deltas, tmp, target_w = agent.w, agent.dw,
-            agent.g, agent.g2,  agent.deltas, agent.tmp, agent.target_w
-        agent.w, agent.dw, agent.g, agent.g2, agent.deltas, agent.tmp, agent.target_w = 
-            nil, nil, nil, nil, nil, nil, nil
-
         local filename = 'save/' .. opt.save_name .. ".t7"
-        torch.save(filename, {agent = agent,
-                                model = agent.network,
+        torch.save(filename, {model = agent.network,
                                 best_model = agent.best_test_network,
                                 test_reward_history = test_reward_history,
                                 reward_history = reward_history,
@@ -242,10 +222,7 @@ while step < opt.steps do
                                 test_history = test_history,
                                 arguments=opt,
                                 step=step})
-        agent.w, agent.dw, agent.g, agent.g2, agent.deltas, agent.tmp, agent.target_w = 
-                w, dw, g, g2, deltas, tmp, target_w
         print('Saved:', filename)
-        io.flush()
         collectgarbage()
     end
 end
